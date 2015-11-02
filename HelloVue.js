970b1ae2475501d54fd84865adc90dab599066a7
Mamba <1160304585@qq.com>
2015-11-02 14:51:39 +0800

    update HelloVue.js

--- HelloVue.js
+++ HelloVue.js
@@ -7,8 +7,13 @@ new Vue({
     },
     methods: {
         MyButton: function () {
+                if("Hello Vue.js!" === this.message){
+                    this.message = this.message.replace('Hello Vue.js!', '刘旭');
+                }
+                 else{
+                    this.message = this.message.replace('刘旭' , 'Hello Vue.js!')
+                }
 
-                this.message = this.message.replace('Hello Vue.js!', '刘旭');
 
 
         }
